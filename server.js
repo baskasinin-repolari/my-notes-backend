47836bd42ec8bad0b4d4784d08a623abbc6a32ca
daniele-frade <63023863+danielefrade@users.noreply.github.com>
2021-07-07 16:46:18 -0400

    Created server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const APP = express();
+const PORT = 3003;
+
+// Middleware
+APP.use(express.json());
+
+// Setup our Mongo connection
+mongoose.connect('mongodb://localhost:27017/my-notes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo :)')
+});
+
+// Configure my cors middleware for other requests
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+APP.use(cors(corsOptions))
+
+const mynotesController = require('./controllers/my-notes')
+APP.use('/my-notes', mynotesController)
+
+APP.listen(PORT, () => {
+    console.log('🎉🎊', 'celebrations happening on port', PORT, '🎉🎊',)
+})
